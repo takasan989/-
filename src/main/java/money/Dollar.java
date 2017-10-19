71da9bb6c1fb6a1ae8fc11d7edfbccaf2f321824
takasan989 <d.0508taka@gmail.com>
2017-10-19 21:31:22 +0900

    TODOを追加
    "$5 * 2 = $10" を完了

--- src/main/java/money/Dollar.java
+++ src/main/java/money/Dollar.java
@@ -1,13 +1,13 @@
 package money;
 
 public class Dollar {
-    int amount;
+    int amount = 10;
 
     Dollar(int amount) {
-
+        this.amount = amount;
     }
 
     void times(int multiplier) {
-
+        amount *= multiplier;
     }
 }
